caa4d8cb4aaa177f132ad186489c7d30f59ed9f7
andrii_solomka <andriissolomka@gmail.com>
2025-05-03 15:19:17 +0300

    Add BotUpdate

---
+++ src/bot/bot.update.ts
@@ -0,0 +1,18 @@
+import { Update, Ctx, Start, On } from 'nestjs-telegraf';
+import { Context } from 'telegraf';
+import { BotService } from './bot.service';
+
+@Update()
+export class BotUpdate {
+  constructor(private readonly botService: BotService) {}
+
+  @Start()
+  async start(@Ctx() ctx: Context) {
+    await this.botService.start(ctx);
+  }
+
+  @On('photo')
+  async onPhoto(@Ctx() ctx: Context) {
+    await this.botService.handlePhoto(ctx);
+  }
+}
